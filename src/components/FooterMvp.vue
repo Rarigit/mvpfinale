18164eaf8607134ee518ffd0bfd5b8550278aa0a
AbuTech <Idsrari@outlook.com>
2023-03-09 21:04:08 -0700

    Initalized setup, vuetify, axios and first request to backend succesfully

---
+++ src/components/FooterMvp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <v-footer color="#40C4FF">
+        <v-row justify="center" no-gutters>
+        <v-btn
+            v-for="link in links"
+            :key="link"
+            color="white"
+            variant="text"
+            class="mx-2"
+            rounded="xl"
+        >
+            {{ link }}
+        </v-btn>
+        <v-col class="text-center mt-4" cols="12">
+            {{ new Date().getFullYear() }} — <strong>Rari Design LLC.</strong>
+        </v-col>
+        </v-row>
+    </v-footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FooterMvp",
+        data: () => ({
+            links: [
+                'News',
+                'Sign-up',
+                'Predict',
+                'Watch-list',
+            ],
+        }),
+    }
+</script>
+
+<style scoped>
+    v-footer{
+        position: fixed;
+    }
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-app id="home">
+    <v-container>
+      <h1>BETCOIN APP</h1>
+      <br>
+      <div>
+        {{ marketBtc }}
+      </div>
+      <!-- <div v-for="market in marketBtc" :key="market.id">
+        {{ market.ath }} 
+        {{ data.atl }}
+        {{ data.current_price }}
+        {{ data.circulating_supply }}
+        {{ data.market-cap_rank }}
+        {{ data.name }}
+        {{ data.total_supply }} 
+      </div>  -->
+    </v-container>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <FooterMvp/>
+  </v-app>
+</template>
+
+<script>
+import FooterMvp from "@/components/FooterMvp.vue"
+import axios from 'axios'
+
+  export default {
+    name: "MainPage",
+    components: {
+      FooterMvp,
+    },
+    data() {
+      return {
+        url: process.env.VUE_APP_API_URL,
+        marketBtc : []
+      }
+    },
+    methods: {
+      getBitcoin() {
+        axios.request({
+          method: "GET",
+          url: this.url + "/market-btc"
+        }).then((response)=>{
+          this.marketBtc = response.data
+        }).catch((error)=>{
+          console.log(error);
+        })
+      }
+    },
+    mounted () {
+      this.getBitcoin();
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
